3e7fc1b753a08ee8996c20391271cc71336a6220
Sangjae Bae <sbae@sv870165lx.am.mds.honda.com>
2019-06-05 08:48:44 -0700

    adding vehicles in the simulation with paramters

--- src/prac.jl
+++ src/prac.jl
@@ -4,35 +4,27 @@ using Distributions
 using Interact
 
 roadway = gen_stadium_roadway(3)
-
-scene = Scene()
-push!(scene, Vehicle(VehicleState(VecSE2(0.0,-DEFAULT_LANE_WIDTH,0.0),roadway,30.0), VehicleDef(),1))
-push!(scene, Vehicle(VehicleState(VecSE2(43.0,0.0,0.0),roadway,20.0), VehicleDef(),2))
-push!(scene, Vehicle(VehicleState(VecSE2(80.0,-DEFAULT_LANE_WIDTH,0.0),roadway,20.0), VehicleDef(),3))
-push!(scene, Vehicle(VehicleState(VecSE2(80.0,0.0,0.0),roadway,20.0), VehicleDef(),4))
-push!(scene, Vehicle(VehicleState(VecSE2(10.0,0.0,0.0),roadway,40.0), VehicleDef(),5))
-car_colors = get_pastel_car_colors(scene)
-cam = FitToContentCamera()
-# render(scene, roadway, cam=cam, car_colors=car_colors)
-
-
+num_vehs = 11
 timestep = 0.2
 
-models = Dict{Int, DriverModel}()
-# models[1] = IntelligentDriverModel()
-# models[2] = IntelligentDriverModel()
-# models[3] = IntelligentDriverModel()
-models[1] = BafflingDriver(timestep)
-models[2] = BafflingDriver(timestep)
-models[3] = BafflingDriver(timestep)
-models[4] = BafflingDriver(timestep)
-models[5] = BafflingDriver(timestep)
+scene = Scene()
+for i in 1:num_vehs
+    if i&3 == 0
+        push!(scene, Vehicle(VehicleState(VecSE2(0.0+10.0*(i-1),-DEFAULT_LANE_WIDTH,0.0),roadway,30.0), VehicleDef(),i))
+    elseif i&3 == 1
+        push!(scene, Vehicle(VehicleState(VecSE2(0.0+10.0*(i-1),0.0,0.0),roadway,30.0), VehicleDef(),i))
+    else
+        push!(scene, Vehicle(VehicleState(VecSE2(0.0+10.0*(i-1),-2*DEFAULT_LANE_WIDTH,0.0),roadway,30.0), VehicleDef(),i))
+    end
+end
+car_colors = get_pastel_car_colors(scene)
+cam = FitToContentCamera()
 
-set_desired_speed!(models[1], 50.0)
-set_desired_speed!(models[2], 40.0)
-set_desired_speed!(models[3], 20.0)
-set_desired_speed!(models[4], 40.0)
-set_desired_speed!(models[5], 30.0)
+models = Dict{Int, DriverModel}()
+for i in 1:num_vehs
+    models[i] = BafflingDriver(timestep)
+    set_desired_speed!(models[i], 40.0)
+end
 
 nticks = 200
 rec = SceneRecord(nticks+1, timestep)
