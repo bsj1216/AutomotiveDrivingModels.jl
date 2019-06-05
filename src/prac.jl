1b295a01a2b86ca14a1ee0d83e99100d68b484ba
Sangjae Bae <sbae@sv870165lx.am.mds.honda.com>
2019-06-04 18:02:43 -0700

    upated

--- src/prac.jl
+++ src/prac.jl
@@ -7,13 +7,13 @@ roadway = gen_stadium_roadway(3)
 
 scene = Scene()
 push!(scene, Vehicle(VehicleState(VecSE2(0.0,-DEFAULT_LANE_WIDTH,0.0),roadway,30.0), VehicleDef(),1))
-push!(scene, Vehicle(VehicleState(VecSE2(23.0,-DEFAULT_LANE_WIDTH,0.0),roadway,20.0), VehicleDef(),2))
-push!(scene, Vehicle(VehicleState(VecSE2(40.0,-DEFAULT_LANE_WIDTH,0.0),roadway,20.0), VehicleDef(),3))
+push!(scene, Vehicle(VehicleState(VecSE2(43.0,-DEFAULT_LANE_WIDTH,0.0),roadway,20.0), VehicleDef(),2))
+push!(scene, Vehicle(VehicleState(VecSE2(80.0,-DEFAULT_LANE_WIDTH,0.0),roadway,20.0), VehicleDef(),3))
 car_colors = get_pastel_car_colors(scene)
 cam = FitToContentCamera()
 # render(scene, roadway, cam=cam, car_colors=car_colors)
 
-timestep = 0.1
+timestep = 0.2
 
 models = Dict{Int, DriverModel}()
 # models[1] = IntelligentDriverModel()
@@ -21,10 +21,10 @@ models = Dict{Int, DriverModel}()
 # models[3] = IntelligentDriverModel()
 models[1] = BafflingDriver(timestep)
 models[2] = BafflingDriver(timestep)
-models[3] = IntelligentDriverModel()
+models[3] = BafflingDriver(timestep)
 
-set_desired_speed!(models[1], 40.0)
-set_desired_speed!(models[2], 20.0)
+set_desired_speed!(models[1], 50.0)
+set_desired_speed!(models[2], 40.0)
 set_desired_speed!(models[3], 20.0)
 
 nticks = 200
